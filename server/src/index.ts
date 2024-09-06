cc2ba9bdd4fc8a365e453fe980c00239b6920c1e
Michael Scuteri <scuteri.michael@gmail.com>
2024-09-06 14:30:50 -0400

    static content

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,18 +3,19 @@ import path from "path";
 import fs from "fs";
 import { filesRoutes } from "./routes/files/files";
 import { fastifyStatic } from "@fastify/static";
+import { FileService } from "./services/fileService";
 
 const server = fastify();
-// export const staticAlbumDir = path.join(__dirname, "..", "albums");
-// console.log(`root static dir: ${staticAlbumDir}`);
-// if (!fs.existsSync(staticAlbumDir)) {
-//   throw "dir does not exist";
-// }
+export const staticAlbumDir = FileService.getAlbumLocation();
+console.log(`root static dir: ${staticAlbumDir}`);
+if (!fs.existsSync(staticAlbumDir)) {
+  throw "dir does not exist";
+}
 
-// server.register(fastifyStatic, {
-//   root: staticAlbumDir,
-//   prefix: "/albums/",
-// });
+server.register(fastifyStatic, {
+  root: staticAlbumDir,
+  prefix: "/albums/",
+});
 server.register(filesRoutes);
 
 server.listen({ port: 80, host: "0.0.0.0" }, (err, address) => {
@@ -25,10 +26,10 @@ server.listen({ port: 80, host: "0.0.0.0" }, (err, address) => {
   console.log(`Server listening at ${address}`);
 });
 
-server.listen({ port: 443, host: "0.0.0.0" }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+// server.listen({ port: 443, host: "0.0.0.0" }, (err, address) => {
+//   if (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+//   console.log(`Server listening at ${address}`);
+// });
