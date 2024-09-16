0c6d7429ea62d01f5424678f330b7e5669cae58e
Michael Scuteri <scuteri.michael@gmail.com>
2024-09-16 01:34:18 -0400

    bug fixes

--- server/src/routes/files/files.ts
+++ server/src/routes/files/files.ts
@@ -35,7 +35,11 @@ const getTest: RouteShorthandOptionsWithHandler = {
     },
   },
   handler: async (req, reply) => {
-    reply.send({ test: FileService.getAlbumLocation() ?? "ahh" });
+    try {
+      reply.send({ test: FileService.getAlbumLocation() ?? "ahh" });
+    } catch (e) {
+      reply.send(e);
+    }
   },
 };
 
@@ -55,32 +59,36 @@ const getAlbums: RouteShorthandOptionsWithHandler = {
     },
   },
   handler: (req, reply) => {
-    const albumsDir = FileService.getAlbumLocation();
-    const albumNames = fs.readdirSync(albumsDir, {
-      recursive: false,
-      encoding: null,
-    });
-    const resp = albumNames.map((albumName) => {
-      let thumbNail = "";
-      const thumbNailPath = path.resolve(albumName, "_thumbnail.jpg");
-      const thumbNailExists = fs.existsSync(thumbNailPath);
-      if (thumbNailExists) {
-        return thumbNailPath;
-      } else {
-        const albumLocation = path.resolve(albumsDir, albumName);
-        const albumPhotos = fs.readdirSync(albumLocation, {
-          recursive: false,
-          encoding: null,
-        });
-        thumbNail = albumPhotos.length >= 0 ? albumPhotos[0] : "";
-      }
+    try {
+      const albumsDir = FileService.getAlbumLocation();
+      const albumNames = fs.readdirSync(albumsDir, {
+        recursive: false,
+        encoding: null,
+      });
+      const resp = albumNames.map((albumName) => {
+        let thumbNail = "";
+        const thumbNailPath = path.resolve(albumName, "_thumbnail.jpg");
+        const thumbNailExists = fs.existsSync(thumbNailPath);
+        if (thumbNailExists) {
+          return thumbNailPath;
+        } else {
+          const albumLocation = path.resolve(albumsDir, albumName);
+          const albumPhotos = fs.readdirSync(albumLocation, {
+            recursive: false,
+            encoding: null,
+          });
+          thumbNail = albumPhotos.length >= 0 ? albumPhotos[0] : "";
+        }
 
-      return {
-        albumName,
-        thumbNail,
-      };
-    });
-    reply.send(resp);
+        return {
+          albumName,
+          thumbNail,
+        };
+      });
+      reply.send(resp);
+    } catch (e) {
+      reply.send(e);
+    }
   },
 };
 
@@ -89,38 +97,84 @@ const getAlbumFiles: RouteShorthandOptionsWithHandler = {
     response: {
       200: {
         type: "array",
-        additinoalProperties: true,
+        items: {
+          type: "object",
+          properties: {
+            fileName: { type: "string" },
+            exifData: {
+              type: "object",
+              properties: {
+                image: {
+                  type: "object",
+                  properties: {
+                    Make: { type: "string" },
+                    Model: { type: "string" },
+                    Orientation: { type: "integer" },
+                    Software: { type: "string" },
+                  },
+                  required: [],
+                },
+                exif: {
+                  type: "object",
+                  properties: {
+                    FocalLength: { type: "string" },
+                    ExposureTime: { type: "string" },
+                    FNumber: { type: "string" },
+                    ISO: { type: "integer" },
+                    DateTimeOriginal: { type: "string", format: "date-time" },
+                    PixelXDimension: { type: "integer" },
+                    PixelYDimension: { type: "integer" },
+                  },
+                  required: [],
+                },
+              },
+              required: [],
+            },
+          },
+          required: ["fileName", "exifData"],
+        },
       },
     },
   },
   handler: async (req, reply) => {
-    const albumName = (req.params as any).albumName as string;
-    const albumsDir = FileService.getAlbumLocation();
-    const albumPath = path.resolve(albumsDir, albumName);
-    const files = fs.readdirSync(albumPath);
-    const filesWithExif: FileNameWithExif[] = [];
-    for (var file of files) {
-      const relativeFilePath = path.join(albumPath, file);
-      let exif = await getExifData(relativeFilePath);
-      if (exif.image.PrintIM) {
-        delete exif.image.PrintIM;
+    try {
+      const albumName = (req.params as any).albumName as string;
+      if (!albumName) {
+        reply.statusCode = 400;
+        reply.send("missing album name in path");
+        return;
       }
-      if (exif.exif.MakerNote) {
-        delete exif.exif.MakerNote;
+      const albumsDir = FileService.getAlbumLocation();
+      const albumPath = path.resolve(albumsDir, albumName);
+      const files = fs.readdirSync(albumPath);
+      const filesWithExif: FileNameWithExif[] = [];
+      for (var file of files) {
+        if (file.startsWith("._") || file.startsWith(".DS_Store")) {
+          continue;
+        }
+        const relativeFilePath = path.join(albumPath, file);
+        let exif = await getExifData(relativeFilePath);
+        if (exif.image.PrintIM) {
+          delete exif.image.PrintIM;
+        }
+        if (exif.exif.MakerNote) {
+          delete exif.exif.MakerNote;
+        }
+        if (exif.exif.UserComment) {
+          delete exif.exif.UserComment;
+        }
+        if (exif.gps) {
+          delete exif.gps;
+        }
+        filesWithExif.push({
+          fileName: file,
+          exifData: exif,
+        });
       }
-      if (exif.exif.UserComment) {
-        delete exif.exif.UserComment;
-      }
-      if (exif.gps) {
-        delete exif.gps;
-      }
-      filesWithExif.push({
-        fileName: file,
-        exifData: exif,
-      });
+      reply.send(filesWithExif);
+    } catch (e) {
+      reply.send(e);
     }
-
-    reply.send(filesWithExif);
   },
 };
 
@@ -138,27 +192,31 @@ const getPhoto: RouteShorthandOptionsWithHandler = {
     },
   },
   handler: async (req, reply) => {
-    const albumName = (req.params as any).albumName as string;
-    const photoName = (req.params as any).photoName as string;
+    try {
+      const albumName = (req.params as any).albumName as string;
+      const photoName = (req.params as any).photoName as string;
 
-    const filepath = path.resolve(
-      FileService.getAlbumLocation(),
-      albumName,
-      photoName
-    );
+      const filepath = path.resolve(
+        FileService.getAlbumLocation(),
+        albumName,
+        photoName
+      );
 
-    const stream = fs.readFileSync(filepath);
-    // // const data = await getExifData(filepath);
-    // // const height = (req.query as any).height ?? data.exif.PixelYDimension;
-    // // const width = (req.query as any).height ?? data.exif.PixelXDimension;
+      const stream = fs.readFileSync(filepath);
+      // // const data = await getExifData(filepath);
+      // // const height = (req.query as any).height ?? data.exif.PixelYDimension;
+      // // const width = (req.query as any).height ?? data.exif.PixelXDimension;
 
-    // // const file = await compressPhoto(filepath, height, width);
-    // // const buffer = file[0].pixels;
+      // // const file = await compressPhoto(filepath, height, width);
+      // // const buffer = file[0].pixels;
 
-    // reply.header("Content-Type", "image/jpeg");
+      // reply.header("Content-Type", "image/jpeg");
 
-    reply.type("image/jpeg");
-    reply.send(stream);
+      reply.type("image/jpeg");
+      reply.send(stream);
+    } catch (e) {
+      reply.send(e);
+    }
   },
 };
 
